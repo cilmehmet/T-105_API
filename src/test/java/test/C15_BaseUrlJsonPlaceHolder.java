e4aa304555de59ffe0f465a34de0ec7df0dc60f4
Mehmetcil <mehmetcil096@gmail.com>
2023-02-04 18:13:41 +0300

    03.02.2023

--- src/test/java/test/C15_BaseUrlJsonPlaceHolder.java
+++ src/test/java/test/C15_BaseUrlJsonPlaceHolder.java
@@ -6,8 +6,7 @@ import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 
 public class C15_BaseUrlJsonPlaceHolder extends JsonPlaceHolderBaseUrl {
 
@@ -95,6 +94,27 @@ public class C15_BaseUrlJsonPlaceHolder extends JsonPlaceHolderBaseUrl {
             request gonderdigimizde donen response’un status code’unun 200 oldugunu ve
             response body’sinin null oldugunu test edin
          */
+        // 1 - URL ve body hazirligi
+
+        specJsonPlace.pathParams("pp1","posts","pp2",50);
+
+        // 2 - Expected Data hazirla
+        // 3 - Response'u kaydet
+
+        Response response = given().
+                spec(specJsonPlace).
+                when().
+                delete("/{pp1}/{pp2}");
+
+        response.prettyPrint();
+
+        // 4 - Assertion
+
+        response.
+                then().
+                assertThat().
+                statusCode(200).
+                body("body",nullValue());
 
     }
 }
